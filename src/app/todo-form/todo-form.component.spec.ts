d13639130286eab77f1870c18534e8cd9c99ab24
Tauhidul Islam Tuhin <abdurshahin5@gmail.com>
2019-11-18 15:39:35 +0600

    added form testing

--- src/app/todo-form/todo-form.component.spec.ts
+++ src/app/todo-form/todo-form.component.spec.ts
@@ -1,8 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoFormComponent } from './todo-form.component';
+import { FormBuilder } from '@angular/forms';
 
 describe('TodoFormComponent', () => {
-  let component: TodoFormComponent;
+
+  var component: TodoFormComponent;
+  beforeEach(()=>{
+    component = new TodoFormComponent(new FormBuilder());
+  });
+
+  it('should create a form with 2 controls',()=>{
+    const result = component.form.contains('name');
+    expect(result).toBeTruthy();
+  });
+
+  it('should make the name control required',()=>{
+    let control = component.form.get('name');
+    control.setValue('');
+
+    expect(control.valid).toBeFalsy();
+  });
+
   
 });
